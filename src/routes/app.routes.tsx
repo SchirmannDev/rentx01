4566f1a7aca0536a5b78665c60a0e28a03ef297c
Cintia Schirmann <cintia.schirmann@gmail.com>
2022-01-15 19:19:26 -0300

    feat: tela de confirmacao criada

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Home, CarDetail, Scheduling, SchedulingDetails } from '../screens';
+import {
+  Home,
+  CarDetail,
+  Scheduling,
+  SchedulingDetails,
+  SchedulingComplete,
+} from '../screens';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 const Routes = () => {
   return (
-    <Navigator initialRouteName="SchedulingDetails">
+    <Navigator initialRouteName="SchedulingComplete">
       <Screen options={{ headerShown: false }} name="Home" component={Home} />
       <Screen
         options={{ headerShown: false }}
@@ -24,6 +30,11 @@ const Routes = () => {
         options={{ headerShown: false }}
         name="SchedulingDetails"
         component={SchedulingDetails}
+      />
+      <Screen
+        options={{ headerShown: false }}
+        name="SchedulingComplete"
+        component={SchedulingComplete}
       />
     </Navigator>
   );
